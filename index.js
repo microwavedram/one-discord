f5b7392ac0f571f4c4a1eb3238950ccd68ed6521
microwavedram <89029844+microwavedram@users.noreply.github.com>
2021-11-25 17:47:56 +0000

    commit #1

---
+++ index.js
@@ -0,0 +1,40 @@
+const colors = require('colors')
+const fs = require('fs')
+const crypto = require('crypto');
+const mongoose = require('mongoose')
+
+const { EventEmitter } = require('stream')
+const { Client, Intents, MessageEmbed } = require('discord.js');
+
+require('dotenv').config()
+
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS
+    ]
+})
+
+function key(password, salt) {
+    const decipher = crypto.createDecipheriv('aes-256-cbc', crypto.scryptSync(password, salt, 32), Buffer.alloc(16, 0));
+    let decrypted = decipher.update(process.env.TOKEN, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+    return decrypted
+}
+
+function bot(token) {
+    return new Promise((resolve,reject) => {
+        client.on('ready', () => {
+            resolve()
+        })
+
+        client.login(key(process.env.PASSWORD, process.env.SALT))
+    })
+    
+}
+
+async function main() {
+    await bot(process.env.TOKEN)
+    console.log("Bot has sucessfuly connected to discord")
+}
+
+main()
